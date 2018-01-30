1babafce9cddf866fd5fd3cfa7eb9893453065c2
kimchangyu <cdit2464@gmail.com>
2018-01-30 15:51:37 +0900

    Journal class Format
    저널 파싱 메소드를 클래스로 묶어놓았습니다.

--- SQLite_FileCarving_TUTORIAL/JournalParsing.py
+++ SQLite_FileCarving_TUTORIAL/JournalParsing.py
@@ -1,64 +1,52 @@
 import re
 from enum import IntEnum
 
+
 class JournalTypeEnum(IntEnum):
     Descriptor = 1
     Block_Commit = 2
     Journal_v1 = 3
     Journal_v2 = 4
     revocation = 5
-    
-class FileConnector:
-    def __init__(self, FileName):
-        try:
-            self.file = open(FileName,'r')
-        except IOError:
-            print('파일이 없습니다.')
 
-    def read_logdump_file(self):
-        with self.file as journal_file:
-            for str in journal_file:
-                try:
-                    print(str)
-                    parse_one_line_text(str)
-                except RuntimeError as error:
-                    print(error)
+class JournalParser:
+    def __init__(self):
+        self.journal_start_block = 0
+        self.transaction_number = 0
 
+        self.journal_sequence = 0
+        self.journal_type = 0
+        self.journal_type_name = ""
 
-def parse_one_line_text(input_text):
-    original_text = input_text
-    journal_info = {}
-    if input_text is '':
-        return
-    if 'transaction' in original_text:
-        (journal_start_block, transaction_number) = parse_journal_head(original_text)
-        print('해당 저널은 {0}번 블록에서 시작하며,  {1}개수의 트랜잭션을 저널링했습니다.'.format(journal_start_block, transaction_number))
-        return
-    if 'end of journal' in original_text:
-        #(end_transaction_number, journal_end_block) = parse_journal_tale(original_text)
-        return
+    def parse_journal_head(self, input_text):
+        original_text = input_text
+        journal_head_parser = re.compile(r'\d+')
+        parser_result = journal_head_parser.findall(original_text)
+        journal_start_block = parser_result[0]
+        transaction_number = parser_result[1]
 
-    # 먼저 저널링 순서를 나타내는 문장을 파싱합니다. 이를 위해 앞 부분 문장을 제거합니다.
-    trimmed_text1 = original_text.strip('Found expected sequence')
-    journal_sequence_parser = re.compile(r'\d+')
-    parser_result = journal_sequence_parser.findall(trimmed_text1)
-    print(parser_result)
-    journal_sequence = parser_result[0]
-    JournalType = parser_result[1]
-    JournalType_name = JournalTypeEnum(int(JournalType)).name
+        return journal_start_block, transaction_number
 
-    print('저널 {0}번은 {1} 타입의 {2}번 블록을 가리킵니다.'.format(journal_sequence, JournalType_name,parser_result[2]))
+    def parse_one_line(self, input_text):
+        original_text = input_text
+        journal_info = {}
+        if input_text is '':
+            return
+        if 'transaction' in original_text:
+            (journal_start_block, transaction_number) = self.parse_journal_head(original_text)
+            print('해당 저널은 {0}번 블록에서 시작하며,  {1}개수의 트랜잭션을 저널링했습니다.'.format(journal_start_block, transaction_number))
+            return
+        if 'end of journal' in original_text:
+            # (end_transaction_number, journal_end_block) = parse_journal_tale(original_text)
+            return
 
+        # 먼저 저널링 순서를 나타내는 문장을 파싱합니다. 이를 위해 앞 부분 문장을 제거합니다.
+        trimmed_text1 = original_text.strip('Found expected sequence')
+        journal_sequence_parser = re.compile(r'\d+')
+        parser_result = journal_sequence_parser.findall(trimmed_text1)
+        print(parser_result)
+        self.journal_sequence = parser_result[0]
+        self.journal_type = parser_result[1]
+        self.journal_type_name = JournalTypeEnum(int(self.journal_type)).name
 
-def parse_journal_head(input_text):
-    original_text = input_text
-    journal_head_parser = re.compile(r'\d+')
-    parser_result = journal_head_parser.findall(original_text)
-    journal_start_block = parser_result[0]
-    transaction_number = parser_result[1]
-
-    return journal_start_block, transaction_number
-
-
-File = FileConnector('logdump1.txt')
-File.read_logdump_file()
+        print('저널 {0}번은 {1} 타입의 {2}번 블록을 가리킵니다.'.format(self.journal_sequence, self.journal_type_name, parser_result[2]))
--- SQLite_FileCarving_TUTORIAL/SQLite_file_carving.py
+++ SQLite_FileCarving_TUTORIAL/SQLite_file_carving.py
@@ -2,24 +2,26 @@
     그 첫번째는 기존에 있던 SQLite file carving 코드를 분석, 연구해서
     SQLite file carving의 시그니처 기반 카빙을 만드는 것입니다. """
 
-import sys,os,re
+import sys,os,re,JournalParsing
 class FileConnector:
     """ 기존 파일은 다양한 파일시스템에서 사용 가능하지 못할듯 합니다.
         특히 전체 디스크 이미지를 분석하는 경우 같은 특이 사항을 대비한 코드가 필요합니다.
-        해당 사항을 빨리 적용해야합니다.
-        """
-    
-def __init__(self, FileName):
-    srcName = FileName
-    if (FileName.contain(".journal")):
-        filetype = 'journal'
-    elif (FileName.contain(".dd")):
-        filetype = 'dd'
-    elif (FileName.contain(".image")):
-        filetype = 'image'
+        해당 사항을 적용해야합니다.
+    """
+    def __init__(self, FileName):
+        srcName = FileName
+        if FileName.contain(".journal"):
+            self.file_type = 'journal'
+            self.fileConnector = JournalParsing.FileConnector(FileName)
+        elif FileName.contain(".dd"):
+            self.file_type = 'dd'
+        elif FileName.contain(".image"):
+            self.file_type = 'image'
 
-    try:
-        file = open(FileName)
-    except Exception as error:
-        print("An exception happened: " + str(error))
+        try:
+            file = open(FileName)
+        except IOError as error:
+            print("파일이 존재하지않습니다.")
+        except Exception as error:
+            print("An exception happened: " + str(error))
 
